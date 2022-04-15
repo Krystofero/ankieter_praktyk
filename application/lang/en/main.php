ebb7952f4a42d3308ae6ed10e99963bd03c977e4
Krystofero <krzysztofosiejewski@gmail.com>
2022-04-15 14:08:32 +0200

    Konfiguracja powiadomień i jquery

---
+++ application/lang/en/main.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    'columns' => [
+        'actions' => 'Akcje'
+    ],
+    'messages' => [
+      'delete_confirm' => 'Czy na pewno chcesz usunąć rekord?'
+    ],
+    'button' => [
+        'save' => 'Zapisz',
+        'add' => 'Dodaj',
+    ],
+    'user' => [
+        'index_title' => 'Lista użytkowników',
+        'status' => [
+            'delete' => [
+                'success' => 'Użytkownik usunięty!'
+            ],
+        ],
+    ],
+    'user' => [
+        'add_title' => 'Dodawanie użytkownika',
+        'edit_title' => 'Edycja użytkownika: :firstname :lastname',
+        'show_title' => 'Podgląd użytkownika',
+        'index_title' => 'Lista użytkowników',
+        'status' => [
+            'store' => [
+                'success' => 'Użytkownik zapisany!'
+            ],
+            'update' => [
+                'success' => 'Użytkownik zaktualizowany!'
+            ],
+            'delete' => [
+                'success' => 'Użytkownik usunięty!'
+            ],
+        ],
+        'fields' => [
+            'firstname' => 'Imię',
+            'lastname' => 'Nazwisko',
+            'email' => 'Email'
+        ]
+    ]
+];
