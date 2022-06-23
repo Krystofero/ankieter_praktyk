67ddaa4a37324e14a8da203400e461a16e71748d
Krystofero <krzysztofosiejewski@gmail.com>
2022-06-23 23:05:10 +0200

    Infocookies

--- application/resources/views/infocookies.blade.php
+++ application/resources/views/infocookies.blade.php
@@ -8,16 +8,26 @@
     <div class="maindiv">
         @include('constant/navigationBar')
         <div class="card">
-            <h3><span>Pliki cookies</span></h3>
-
-            Gdy korzystasz z naszych serwisów, gromadzimy informacje o Twojej wizycie i sposobie poruszania się w naszych serwisach. W tym celu stosujemy pliki cookies. Plik cookies zawiera dane informatyczne, które są umieszczone w Twoim urządzeniu końcowym - przeglądarce internetowej, z której korzystasz.
-
-            Pliki cookies używane w naszych serwisach wykorzystywane są między innymi do bieżącej optymalizacji serwisów oraz ułatwiania Twojego z nich korzystania. Niektóre funkcjonalności dostępne w naszych serwisach mogą nie działać, jeżeli nie wyrazisz zgody na instalowanie plików cookies.
-
-            Instalowanie plików cookies lub uzyskiwanie do nich dostępu nie powoduje zmian w Twoim urządzeniu ani w oprogramowaniu zainstalowanym na tym urządzeniu.
-
-            Stosujemy dwa rodzaje plików cookies: sesyjne i trwałe. Pliki sesyjne wygasają po zakończonej sesji, której czas trwania i dokładne parametry wygaśnięcia określa używana przez Ciebie przeglądarka internetowa oraz nasze systemy analityczne. Trwałe pliki cookies nie są kasowane w momencie zamknięcia okna przeglądarki, głównie po to, by informacje o dokonanych wyborach nie zostały utracone. Pliki cookies aktywne długookresowo wykorzystywane są, aby pomóc nam wspierać komfort korzystania z naszych serwisów, w zależności od tego czy dochodzi do nowych, czy do ponownych odwiedzin serwisu.
-
+            <div class="container-fluid text-md-left">
+                <div class="row">
+                    <h3><span>Pliki cookies</span></h3>
+                    Gdy korzystasz z naszych serwisów, gromadzimy informacje o Twojej wizycie i sposobie poruszania się w naszych serwisach. W tym celu stosujemy pliki cookies. Plik cookies zawiera dane informatyczne, które są umieszczone w Twoim urządzeniu końcowym - przeglądarce internetowej, z której korzystasz.
+                    <br>Pliki cookies używane w naszych serwisach wykorzystywane są między innymi do bieżącej optymalizacji serwisów oraz ułatwiania Twojego z nich korzystania. Niektóre funkcjonalności dostępne w naszych serwisach mogą nie działać, jeżeli nie wyrazisz zgody na instalowanie plików cookies.
+                    <br>Instalowanie plików cookies lub uzyskiwanie do nich dostępu nie powoduje zmian w Twoim urządzeniu ani w oprogramowaniu zainstalowanym na tym urządzeniu.
+                    <br>Stosujemy dwa rodzaje plików cookies: sesyjne i trwałe. Pliki sesyjne wygasają po zakończonej sesji, której czas trwania i dokładne parametry wygaśnięcia określa używana przez Ciebie przeglądarka internetowa oraz nasze systemy analityczne. Trwałe pliki cookies nie są kasowane w momencie zamknięcia okna przeglądarki, głównie po to, by informacje o dokonanych wyborach nie zostały utracone. Pliki cookies aktywne długookresowo wykorzystywane są, aby pomóc nam wspierać komfort korzystania z naszych serwisów, w zależności od tego czy dochodzi do nowych, czy do ponownych odwiedzin serwisu.
+                </div>
+                <br>
+                <div class="row">
+                    <h3><span>Polityka prywatności</span></h3>              
+                    Bezpieczeństwo informacji, ze szczególnym uwzględnieniem ochrony danych osobowych i poszanowania prawa do prywatności ma nadrzędne znaczenie.
+                    
+                    W celu jak najlepszej ochrony Państwa danych osobowych, strona stosuje następujące zasady:
+                    <br>-strona przetwarza dane osobowe wyłącznie dla realizacji prawnie uzasadnionych celów, dla jakich je gromadzi lub w sposób określony w bezwzględnie obowiązujących przepisach prawa.
+                    <br>-strona przetwarza tylko takie kategorie danych osobowych, jakie są niezbędne dla realizacji celów przetwarzania.
+                    <br>-strona nie przetwarza danych osobowych w sposób niezgodny z celem ich przetwarzania.
+                </div>
+            </div>
+            
         </div>
     </div>
     @include('constant/footer')
