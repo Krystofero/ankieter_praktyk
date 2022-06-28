89708df337a37c810292d49b12167a4fcde073b1
Krystofero <krzysztofosiejewski@gmail.com>
2022-06-29 00:50:48 +0200

    zmiany w fabryce

--- application/database/factories/UserFactory.php
+++ application/database/factories/UserFactory.php
@@ -21,8 +21,7 @@ class UserFactory extends Factory
             'firstname' => $this->faker->firstname(),
             'lastname' => $this->faker->lastname(),
             'email' => $this->faker->unique()->safeEmail(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password' => '$5y$27$ahfsdhbxcvnbyuteozbcxnjjk'
         ];
     }
 
